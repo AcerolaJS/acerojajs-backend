c09fb57c280955a948feb785a6422893a65de384
Fernando Cano <fernando@yeeply.com>
2015-04-20 14:05:37 +0200

    Correciones en el arbol de decision del controlador de sesion

--- controllers/session.js
+++ controllers/session.js
@@ -15,19 +15,32 @@ module.exports.controller = function(app) {
                 "errors": errors
             }, 400);
             return;
-        }
-        UserModel.findOne({
-            "email": req.body.email
-        }, function(err, user) {
-            user.session_token = passwordHash.generate(Config.get('secretword') + Date.now());
-            user.save(function(err) {
-                if (err) {
-                    console.error('ERROR!');
+        } else {
+            UserModel.findOne({
+                "email": req.body.email
+            }, function(err, userToLogin) {
+                console.log(req.body.email);
+                if (!passwordHash.verify(req.body.password, userToLogin.password)) {
+                    res.send({
+                        "errors": ["No valid login "]
+                    }, 401);
+                    return;
+                } else {
+                    UserModel.findOne({
+                        "email": req.body.email
+                    }, function(err, user) {
+                        user.session_token = passwordHash.generate(Config.get('secretword') + Date.now());
+                        user.save(function(err) {
+                            res.send({
+                                "errors": ["An error"]
+                            }, 400);
+                        });
+                        Helpers.response(req, res, user);
+                        return;
+                    });
                 }
             });
-            Helpers.response(req, res, user);
-            return;
-        });
+        }
     };
     SessionController.check = function(req, res, next) {
         var token = req.get('X-Auth-Token')
@@ -37,14 +50,15 @@ module.exports.controller = function(app) {
                 "errors": errors
             }, 400);
             return;
+        } else {
+            UserModel.findOne({
+                "session_token": token
+            }, function(err, user) {
+                if (err) return handleError(err);
+                Helpers.response(req, res, user);
+                return;
+            });
         }
-        UserModel.findOne({
-            "session_token": token
-        }, function(err, user) {
-            if (err) return handleError(err);
-            Helpers.response(req, res, user);
-            return;
-        });
     };
     app.post('/login:ext?/', SessionController.login);
     app.get('/app/v1/check:ext?/', SessionController.check);
